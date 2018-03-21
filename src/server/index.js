9e0f95d9043aa03275af7cef1af396b828a4666b
umitalp <umitalp@gmail.com>
2018-03-21 03:01:46 -0400

    🔧Do we really need this

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,4 @@
-import App from '../client/App';
-import React from 'react';
-import { Provider } from 'react-redux'
-import { StaticRouter } from 'react-router-dom';
 import express from 'express';
-import { renderToString } from 'react-dom/server';
-import configureStore from '../client/redux/store/configureStore'
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
@@ -13,40 +7,26 @@ server
   .disable('x-powered-by')
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
-    const store = configureStore({})
-    const context = {};
-    const markup = renderToString(
-      <Provider store={store}>
-        <StaticRouter context={context} location={req.url}>
-          <App />
-        </StaticRouter>
-      </Provider>
-    );
-
-    if (context.url) {
-      res.redirect(context.url);
-    } else {
-      res.status(200).send(
-        `<!doctype html>
-        <html lang="">
-          <head>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta charset="utf-8" />
-            <title>Welcome to Razzle</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            ${assets.client.css
-              ? `<link rel="stylesheet" href="${assets.client.css}">`
-              : ''}
-            ${process.env.NODE_ENV === 'production'
-              ? `<script src="${assets.client.js}" defer></script>`
-              : `<script src="${assets.client.js}" defer crossorigin></script>`}
-          </head>
-          <body>
-            <div id="root">${markup}</div>
-          </body>
-        </html>`
-      );
-    }
+    res.status(200).send(` 
+      <!doctype html>
+      <html lang="">
+        <head>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta charset="utf-8" />
+          <title>Welcome to Razzle</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          ${assets.client.css
+            ? `<link rel="stylesheet" href="${assets.client.css}">`
+            : ''}
+          ${process.env.NODE_ENV === 'production'
+            ? `<script src="${assets.client.js}" defer></script>`
+            : `<script src="${assets.client.js}" defer crossorigin></script>`}
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+    `);
   });
 
 export default server;
