d27e9b00bc64248045c394634e96299da1c3e2e3
umitalp <umitalp@gmail.com>
2018-04-16 21:36:36 -0400

    Test

--- src/client/containers/developer/Developer.js
+++ src/client/containers/developer/Developer.js
@@ -143,26 +143,6 @@ class Developer extends React.Component {
                 <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
               </FormGroup>
               <FormGroup>
-                <Label for="exampleSelect">Select</Label>
-                <Input type="select" name="select" id="exampleSelect">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
-              </FormGroup>
-              <FormGroup>
-                <Label for="exampleSelectMulti">Select Multiple</Label>
-                <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                </Input>
-              </FormGroup>
-              <FormGroup>
                 <Label for="exampleText">Text Area</Label>
                 <Input type="textarea" name="text" id="exampleText" />
               </FormGroup>
